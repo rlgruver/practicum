2c3197438aeb8501a161f897c401e643303ff593
Rebekah Gruver <bekahlyn92@gmail.com>
2015-10-27 23:45:09 -0600

    new realtime output

--- Website/processOutput.php
+++ Website/processOutput.php
@@ -1,32 +1,12 @@
-<html>
-<head>
-  <!-- Compiled and minified CSS -->
-  <link rel="stylesheet" type="text/css" href="css/theme.css" >
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/css/materialize.min.css"/>
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-  <!-- Compiled and minified JavaScript -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
-
-  <script>
-
-  function redirectPage()
-  {
-   parent.document.getElementById("frame1").src = "writable/output.txt";
- }
- </script>
-</head>
-
-</html>
-
 <?php
+
 $localInputFile = "writable/input.txt"; 
 $localOutputFile = "writable/output.txt"; 
+$localSolutionsFile = "txt/realtime_output.txt";   
 $remoteInputFile = "/code/spopt-stable/tests/input.txt";
 $remoteOutputFile = "/code/spopt-stable/tests/input.txt.out"; 
 $remoteSolutionsFile = "/code/spopt-stable/tests/input.txt.solutions"; 
-$remoteDiscardedFile = "/code/spopt-stable/tests/input.txt.discarded"; 
+ 
 
 set_include_path(get_include_path() . PATH_SEPARATOR . 'phpseclib');
 include('Net/SSH2.php');
@@ -38,6 +18,7 @@ if (!$ssh->login('rlgruver', 'utep$2015')) {
   exit('ssh Login Failed');
 }
 
+
 //sftp connect to cr2g server
 $sftp = new Net_SFTP('cr2g01.cs.utep.edu',22);
 if (!$sftp->login('rlgruver', 'utep$2015')) { 
@@ -45,30 +26,32 @@ if (!$sftp->login('rlgruver', 'utep$2015')) {
 }
 
 
-//remove old output files
-// $sftp->remove($localOutputFile); 
-
 //send input file to cr2g server 
 $sftp->put($remoteInputFile, $localInputFile,  NET_SFTP_LOCAL_FILE);
 $sftp->chmod(0777, $remoteInputFile);
 
 //execute python script 
-$ssh->exec('cd /code/spopt-stable; python SpOpt.py tests/input.txt');
-// $ssh->read('rlgruver@cr2g01:~$');
-// $ssh->write("cd /code/spopt-stable/tests\n");
-// $ssh->read('rlgruver@cr2g01:/code/spopt-stable/tests$');
-// $ssh->write("ls\n"); 
-// echo $ssh->read('/(.*)rlgruver/s', NET_SSH2_READ_REGEX);
+$ssh->write("cd /code/spopt-stable\n");
+$ssh->read('rlgruver@cr2g01:/code/spopt-stable$');
+$ssh->exec("python SpOpt.py tests/input.txt\n");
 
-//get output contents
-  if(!$sftp->get($remoteOutputFile, $localOutputFile)){
+
+
+$starttime = time();
+
+while ($ssh->isConnected()) {
+        $now = time()-$starttime;
+        if($now >$timeoutVar){
+          break;
+        } 
+
+        sleep(3);
+        $sftp->get($remoteSolutionsFile, $localSolutionsFile);
+        
+}
+
+if(!$sftp->get($remoteOutputFile, $localOutputFile)){
     throw new Exception('Unable to get output file.');
   }
-  else{
-    echo "<script>redirectPage();</script>";
-  }
-
-
-
 
 ?>
--- Website/results.php
+++ Website/results.php
@@ -19,14 +19,28 @@
 
   <!-- Compiled and minified JavaScript -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.0/js/materialize.min.js"></script>
-<script>
 
-function displays(){
-document.getElementById('loadImg').style.display='none';
-document.getElementById('frameHolder').style.display='inline';
+  <script>
+  function displays(){
+    document.getElementById('loadImg').style.display='none';
+    document.getElementById('frameHolder').style.display='inline';
 
-}
-</script>
+  }
+  </script>
+
+  <script>
+  function loading(){
+    document.getElementById('loading').style.visibility = 'visible'; 
+  }
+  </script>
+
+  <script>
+  window.setInterval("reloadIFrame();", 5000);
+
+  function reloadIFrame() {
+   document.getElementById('output').contentWindow.location.reload(true);
+ }
+ </script>
 
 </head>
 
@@ -49,56 +63,81 @@ document.getElementById('frameHolder').style.display='inline';
       <h3 class="center-align">Solutions</h3>
 
       <div class='row col s12 center-align'>
-        
-        
-        <img id="loadImg" class = "white"style="border:1.5px solid #99CC00" width="700" height="400" src="img/loading.gif" />
-        <div id="frameHolder" style="display:none">
-          <iframe id="frame1" src= "processOutput.php" class="white" style="border:1.5px solid #99CC00" width="700" height="400"
-          onload="displays()"></iframe>
+        <div id ="loading" style="visibility:hidden;"> <img src="img/loadbar.gif" width="9%" height="auto"> </div>
+        <div id="frameHolder">
+          <iframe id="output" name="output" src= "txt/realtime_output.txt" class="white" style="border:1.5px solid #99CC00" width="700" height="400"></iframe>
         </div>
       </div>
 
     </div>
   </div>
 
- <div class='row col s12 center-align'>
-  <a href='txt/upload.txt' class='modal-action waves-effect waves-green btn' download>Download Input File</a>
-   <a href='writable/output.txt' class='modal-action waves-effect waves-green btn' download>Download Output File</a>
- </div>
- <br>
-
- <footer class="page-footer grey darken-2">
-  <div class="container">
-    <div class="row">
-      <div class="col s1">
-        <img src="img/logo.png">
-      </div>
-      <div class="col s6 offset-s2">
-        <h5 class="white-text">CR2G</h5>
-        <p class="grey-text text-lighten-4">We are a group of people who share interests in all aspects of constraint solving and optimization. We do research on applications of constraints and on discovering new constraint solving and optimization algorithms. We are also involved in educational activities, and in outreach activities.</p>
-      </div>
-      <div class="col s3">
-        <h5 class="white-text">Useful Links</h5>
-        <ul>
-          <li><a class="grey-text text-lighten-3" href="http://cr2g.constraintsolving.com/" target="_blank">CR2G Website</a></li>
-          <li><a class="grey-text text-lighten-3" href="http://www.cs.utep.edu/" target="_blank">UTEP Computer Science</a></li>
-          <li><a class="grey-text text-lighten-3" href="http://www.cs.utep.edu/mceberio/research/" target="_blank">Martine Ceberio</a></li>
-        </ul>
-      </div>
-
-    </div>
+  <div class='row col s12 center-align'>
+    <a href='txt/upload.txt' class='modal-action waves-effect waves-green btn' download>Download Input File</a>
+    <a href='writable/output.txt' class='modal-action waves-effect waves-green btn' download>Download Output File</a>
   </div>
-  <div class="footer-copyright grey darken-3">
+  <br>
+
+  <footer class="page-footer grey darken-2">
     <div class="container">
-      © 2015 Mohandeseen
+      <div class="row">
+        <div class="col s1">
+          <img src="img/logo.png">
+        </div>
+        <div class="col s6 offset-s2">
+          <h5 class="white-text">CR2G</h5>
+          <p class="grey-text text-lighten-4">We are a group of people who share interests in all aspects of constraint solving and optimization. We do research on applications of constraints and on discovering new constraint solving and optimization algorithms. We are also involved in educational activities, and in outreach activities.</p>
+        </div>
+        <div class="col s3">
+          <h5 class="white-text">Useful Links</h5>
+          <ul>
+            <li><a class="grey-text text-lighten-3" href="http://cr2g.constraintsolving.com/" target="_blank">CR2G Website</a></li>
+            <li><a class="grey-text text-lighten-3" href="http://www.cs.utep.edu/" target="_blank">UTEP Computer Science</a></li>
+            <li><a class="grey-text text-lighten-3" href="http://www.cs.utep.edu/mceberio/research/" target="_blank">Martine Ceberio</a></li>
+          </ul>
+        </div>
+
+      </div>
     </div>
-  </div>
-</footer>
+    <div class="footer-copyright grey darken-3">
+      <div class="container">
+        © 2015 Mohandeseen
+      </div>
+    </div>
+  </footer>
 
 </body>
 </html>
 
 
 
+<?php
 
+//remove old files
+$localOutputFile = "writable/output.txt"; 
+$localSolutionsFile = "txt/realtime_output.txt"; 
+
+$f = @fopen($localOutputFile, "r+");
+if ($f !== false) {
+  ftruncate($f, 0);
+  fclose($f);
+}
+
+$f = @fopen($localSolutionsFile, "r+");
+if ($f !== false) {
+  ftruncate($f, 0);
+  fclose($f);
+}
+
+
+$pid = exec( "php processOutput.php > /dev/null &" );
+while(exec("ps $pid")){
+
+  echo "<script> 
+  loading();
+  </script>";
+}
+
+
+?>
 
