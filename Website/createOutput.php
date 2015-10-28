dcfe7f3141f29c006e8cc41c01b1eb334a4a6ce1
Salah Atiyeh <atiyehsalah@gmail.com>
2015-10-28 17:22:09 -0600

    added real time parsing

--- Website/createOutput.php
+++ Website/createOutput.php
@@ -1,58 +1,40 @@
 <?php
-
-
 $text = file_get_contents('txt/test.txt');
-
 $myfile = fopen("writable/troy.txt", "w") or die("Unable to open file!");
-
 $solutionList = explode("SOLUTIONS", $text);
 $solutionIndividual = explode("SOLUTION", $solutionList[1]);
-
-
 $varWrite = false;
 $count = 1;
 foreach($solutionIndividual as $value){
-
 	$nameArray = [];
 	$lowerArray = [];
 	$upperArray = [];
-
 	$objective = explode("OBJECTIVE FUNCTION", $solutionIndividual[$count]);
 	$range = explode("Precision",$objective[1]);
 	$range[0] = str_replace("[", "", $range[0]);
 	$range[0] = str_replace("]", "", $range[0]);
 	$objRanges = explode(",", $range[0]);
-
 	$lowerArray[] = $objRanges[0];
 	$upperArray[] = $objRanges[1];
-
 	$domList = explode("DOMAIN", $range[1]);
 	$doms = explode("Precision", $domList[1]);
-
 	$doms[0] = preg_replace('/\s+/','', $doms[0]);
 	$doms[0] = preg_replace('/in\[/', 'SPLITHERE', $doms[0]);
 	$doms[0] = preg_replace('/]/', 'SPLITHERE', $doms[0]);
-
 	$getVarNames = explode("SPLITHERE", $doms[0]);
 	array_pop($getVarNames);
-
 	$i = 0;
 	while($i < count($getVarNames)){
-
 		$name = $getVarNames[$i];
 		$i ++;
 		$nameArray [] = $name;
-
 		$bounds = explode(",", $getVarNames[$i]);
 		$lower = $bounds[0];
 		$upper = $bounds[1];
-
 		$lowerArray[] = $lower;
 		$upperArray[] = $upper;
 		$i ++;
-
 	}
-
 	if(!$varWrite){
 		$varString = "";
 		for($i = 0; $i < count($nameArray); $i++){
@@ -62,7 +44,6 @@ foreach($solutionIndividual as $value){
 		$varWrite = true;
 		fwrite($myfile, $varString."\n");
 	}
-
 	$txt ="";
 	for($i = 0; $i <count($lowerArray); $i++){
 		$txt .= $lowerArray[$i].',';
@@ -70,7 +51,6 @@ foreach($solutionIndividual as $value){
 	$txt = substr($txt, 0, -1);
 	$txt = preg_replace('/\s+/','', $txt);
 	fwrite($myfile, $txt."\n");
-
 	$txt ="";
 	for($i = 0; $i <count($upperArray); $i++){
 		$txt .= $upperArray[$i].',';
@@ -82,5 +62,4 @@ foreach($solutionIndividual as $value){
 }
 echo "It worked!";
 fclose($myfile);
-
 ?>
